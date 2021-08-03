66c0d462ab1d32394d95630c3c4ac5b89b98afdb
erik <erik19970805@gmail.com>
2021-08-03 12:01:37 -0500

    Se creo los metodos para obtener blogs por categorias

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -1,8 +1,15 @@
 import { Response, Request } from 'express';
+import { Types } from 'mongoose';
 import { IBlog } from '../interfaces/blog.interface';
 import { IReqAuth } from '../interfaces/user.interface';
 import Blogs from '../models/blog.model';
 
+const pagination = (req: IReqAuth) => {
+  const page = Number(req.query.page) * 1 || 1;
+  const limit = Number(req.query.limit) * 1 || 4;
+  const skip = (page - 1) * limit;
+  return { page, limit, skip };
+};
 export const createBlog = async (req: IReqAuth, res: Response): Promise<Response> => {
   try {
     const { title, content, description, thumbnail, category }: IBlog = req.body;
@@ -71,6 +78,64 @@ export const getBlogs = async (req: Request, res: Response): Promise<Response> =
       },
     ]);
     return res.json(blogs);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const getBlogsByCategory = async (req: Request, res: Response): Promise<Response> => {
+  const { limit, skip } = pagination(req);
+  try {
+    const data = await Blogs.aggregate([
+      {
+        $facet: {
+          totalData: [
+            { $match: { category: Types.ObjectId(req.params.id) } },
+            // User
+            {
+              $lookup: {
+                from: 'users',
+                let: { user_id: '$user' },
+                pipeline: [
+                  { $match: { $expr: { $eq: ['$_id', '$$user_id'] } } },
+                  { $project: { password: 0 } },
+                ],
+                as: 'user',
+              },
+            },
+            // array -> object
+            { $unwind: '$user' },
+            // Sorting
+            { $sort: { createdAt: -1 } },
+            { $skip: skip },
+            { $limit: limit },
+          ],
+          totalCount: [
+            { $match: { category: Types.ObjectId(req.params.id) } },
+            { $count: 'count' },
+          ],
+        },
+      },
+      {
+        $project: {
+          count: { $arrayElemAt: ['$totalCount.count', 0] },
+          totalData: 1,
+        },
+      },
+    ]);
+    const blogs = data[0].totalData;
+    const { count } = data[0];
+
+    // Pagination
+    let total = 0;
+
+    if (count % limit === 0) {
+      total = count / limit;
+    } else {
+      total = Math.floor(count / limit) + 1;
+    }
+
+    return res.json({ blogs, total });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createBlog, getBlogs } from '../controllers/blog.controller';
+import { createBlog, getBlogs, getBlogsByCategory } from '../controllers/blog.controller';
 import { auth } from '../middlewares/auth';
 
 const router = Router();
 
 router.route('/').post(auth, createBlog);
 router.route('/home').get(getBlogs);
+router.route('/category/:id').get(getBlogsByCategory);
 
 export default router;
