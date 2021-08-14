e46816ab4de83833b2e4fa6083d2aa1ab519e72b
erik <erik19970805@gmail.com>
2021-08-14 14:42:04 -0500

    Se creo el controlador y la ruta para poder tener acceso a la info de un usuario

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,4 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import { IReqAuth } from '../interfaces/user.interface';
 import Users from '../models/user.model';
@@ -18,13 +18,22 @@ export const resetPassword = async (req: IReqAuth, res: Response): Promise<Respo
   if (!req.user) return res.status(400).json({ message: 'Autenticación Invalida' });
   if (req.user.type !== 'register')
     return res.status(400).json({
-      message: ` Se Inicio sesisión rapida con ${req.user.type}, por ende no puede utilizar esta función`,
+      message: ` Se Inicio sesión rápida con ${req.user.type}, por ende no puede utilizar esta función`,
     });
   try {
     const { password } = req.body;
     const passwordHash = await bcrypt.hash(password, 12);
     await Users.findOneAndUpdate({ _id: req.user._id }, { password: passwordHash });
     return res.json({ message: 'Su contraseña ha sido actualizada' });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const getUser = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const user = await Users.findById(req.params.id).select('-password');
+    return res.json(user);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { resetPassword, updateUser } from '../controllers/user.controller';
+import { resetPassword, updateUser, getUser } from '../controllers/user.controller';
 import { auth } from '../middlewares/auth';
 
 const router = Router();
 router.route('/').patch(auth, updateUser);
 router.route('/reset_password').patch(auth, resetPassword);
+router.route('/:id').get(getUser);
 
 export default router;
